82fd0242a1e3abe314553714c834c569d63d6725
jvijaybhaskar <j.vijaybhaskar@yahoo.com>
2017-03-27 18:31:57 +0800

    updated requirements

--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -12,11 +12,13 @@ class Footer extends React.Component {
 	 					<li> Done:Delete list item </li>
 	 					<li> Done:Update status based on action </li>
 	 					<li> Done:Added react-bootstrap module and basic layout to page </li>
+	 					<li> Limit Entry of items to list </li>
+	 					<li> Provision to add more details to the list (tags, priority, target date) </li>
 	 					<li> Edit bucket list </li>
 						<li> Auto hide status messages</li>
 						<li> Persist/Update list in backend</li>
 						<li> Improve look and feel</li>
-						<li> </li>
+						<li> View/Modify details of individual item</li>
 					</ul>
 				</div>
 			</footer>
